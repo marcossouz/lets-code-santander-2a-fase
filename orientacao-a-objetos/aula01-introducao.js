f758d637b2a1f474a021c7f01de7ec54c8b885f1
Marcos Souza <mrcsz.m@gmail.com>
2021-07-02 11:52:02 -0300

    introduction

---
+++ orientacao-a-objetos/aula01-introducao.js
@@ -0,0 +1,18 @@
+const pessoa1 = { nome : "Carlos", idade : 20 };
+
+const quadrado = { 
+    base : 10, 
+    altura : 10, 
+    calcularArea : () => { return this.base * this.altura; }
+};
+
+
+const agenda = {
+    contatos : [
+        {nome : 'contato1', telefone : 'telefone1', email : 'email1@teste.com'},
+        {nome : 'contato2', telefone : 'telefone2', email : 'email2@teste.com'},
+        {nome : 'contato3', telefone : 'telefone3', email : 'email3@teste.com'},
+        {nome : 'contato4', telefone : 'telefone4', email : 'email4@teste.com'}
+    ],
+    adicionar : function(contato){ this.contatos.push(contato) }
+}
