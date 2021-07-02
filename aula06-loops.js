63dd7fc5a8368b72811a8fed3aad354c8ad12c96
Marcos Souza <mrcsz.m@gmail.com>
2021-07-02 08:30:07 -0300

    loops

---
+++ aula06-loops.js
@@ -0,0 +1,16 @@
+let count = 0; 
+while(count < 100){
+    console.log(count);
+    count++;
+}
+
+let count = 0; 
+do{
+    console.log(count);
+    count++;
+}
+while(count < 100);
+
+for(let i = 0; i < 10; i++){
+    console.log(i+1);
+}
