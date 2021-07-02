3bf6502ee9a09b0b4ca1bec1eba5d4a4f46b1c52
Marcos Souza <mrcsz.m@gmail.com>
2021-07-02 17:44:56 -0300

    reder lists

--- 03-react/meu-app/src/index.js
+++ 03-react/meu-app/src/index.js
@@ -5,11 +5,12 @@ import App2 from './components/App2';
 import App3 from './components/App3';
 import App4 from './components/App4';
 import App5 from './components/App5';
+import Lista from './components/Lista';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App5 />
+    <Lista />
   </React.StrictMode>,
   document.getElementById('root')
 );
