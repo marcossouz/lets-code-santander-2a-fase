e8143c37884a194d845f45deeb6070e2d7c01eca
Marcos Souza <mrcsz.m@gmail.com>
2021-07-02 17:19:47 -0300

    ciclo de vida
    Signed-off-by: Marcos Souza <mrcsz.m@gmail.com>

--- 03-react/meu-app/src/index.js
+++ 03-react/meu-app/src/index.js
@@ -4,11 +4,12 @@ import App from './components/App';
 import App2 from './components/App2';
 import App3 from './components/App3';
 import App4 from './components/App4';
+import App5 from './components/App5';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App4 />
+    <App5 />
   </React.StrictMode>,
   document.getElementById('root')
 );
