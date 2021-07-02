44dd10a4e00611cfd398f989f0a5ab1ca30bc0a7
Marcos Souza <mrcsz.m@gmail.com>
2021-07-02 14:55:59 -0300

    props

--- 03-react/meu-app/src/index.js
+++ 03-react/meu-app/src/index.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
+import App2 from './components/App2';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App2 title='Título'>
+      Conteúdo  
+    </App2>
+
+    <App2 title='Título2'>
+      Conteúdo2  
+    </App2>
   </React.StrictMode>,
   document.getElementById('root')
 );
