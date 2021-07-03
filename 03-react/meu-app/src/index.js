c356f0cc6317e7c01920fde1daf855a93c7445fc
Marcos Souza <mrcsz.m@gmail.com>
2021-07-02 21:54:41 -0300

    formulario
    Signed-off-by: Marcos Souza <mrcsz.m@gmail.com>

--- 03-react/meu-app/src/index.js
+++ 03-react/meu-app/src/index.js
@@ -1,24 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './components/App';
-import App2 from './components/App2';
-import App3 from './components/App3';
-import App4 from './components/App4';
-import App5 from './components/App5';
-import { Lista, Item } from './components/Lista';
+// import App from './components/App';
+// import App2 from './components/App2';
+// import App3 from './components/App3';
+// import App4 from './components/App4';
+// import App5 from './components/App5';
+// import { Lista, Item } from './components/Lista';
+import Formulario from './components/Formulario';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Lista>
-      Test3
-      <Item key={999} id={999} completo={false}>
-        Teste1
-      </Item>
-      <Item key={998} id={998} completo={true}>
-        Teste2
-      </Item>
-    </Lista>
+    <Formulario />
   </React.StrictMode>,
   document.getElementById('root')
 );
